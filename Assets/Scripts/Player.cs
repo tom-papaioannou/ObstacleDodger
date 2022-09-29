6a8817b9202bdfbaab497e6dd681f7ed414e2890
Tom Papaioannou <Tompap2007@hotmail.com>
2022-09-29 15:54:31 +0300

    Modify game to be played on android devices with accelerometer

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,7 @@ public class Player : MonoBehaviour
 {
     Rigidbody rb;
     [SerializeField] private float speed = 1.0f;
+    [SerializeField] private float moveThreshold = 0.2f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +16,13 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rb.AddForce(new Vector3((Input.GetAxis("Horizontal") * speed * Time.deltaTime)/ rb.mass, 0.0f, (Input.GetAxis("Vertical") * speed * Time.deltaTime)/rb.mass));
+#if UNITY_EDITOR
+
+        rb.AddForce(new Vector3((Input.GetAxis("Horizontal") * speed * Time.deltaTime) / rb.mass, 0.0f, (Input.GetAxis("Vertical") * speed * Time.deltaTime) / rb.mass));
+#else
+        float x = Input.acceleration.x > moveThreshold ? Input.acceleration.x - moveThreshold : Input.acceleration.x < -moveThreshold ? Input.acceleration.x + moveThreshold : 0.0f;
+        float z = Input.acceleration.z > moveThreshold ? Input.acceleration.z - moveThreshold : Input.acceleration.z < -moveThreshold ? Input.acceleration.z + moveThreshold : 0.0f;
+        rb.AddForce(new Vector3((x * speed * Time.deltaTime)/ rb.mass, 0.0f, (z * speed * Time.deltaTime)/rb.mass));
+#endif
     }
 }
